296a19133c2325e1442192919e94e55748016034
Lucas <lucas.charrier13@gmail.com>
2019-12-18 12:57:48 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+/* window.onload=function() {
+	// Get the Object by ID
+	var a = document.getElementById("svgObject");
+	// Get the SVG document inside the Object tag
+	var svgDoc = a.contentDocument;
+	// Get one of the SVG items by ID;
+	var svgItem = svgDoc.getElementById("svgItem");
+	// Set the colour to something else
+	svgItem.setAttribute("fill", "lime");
+}; */
+$(document).ready(function () {
+    $(".btn").click(function () {
+        $("form").addClass("hidden");
+        $(".div-jeu").removeClass("hidden");
+    })
+ 
+})
